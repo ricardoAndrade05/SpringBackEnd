3124e4ec123f47a49a2664d220839885c7d06a9d
Ricardo Andrade <ricardo.andrade05@gmail.com>
2022-07-16 19:47:17 -0300

    Ajustando e testando a deleção de clientes.

--- src/main/java/com/nelioalves/coursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/coursomc/services/ClienteService.java
@@ -55,7 +55,7 @@ public class ClienteService {
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possivel exlcuir porque há entidades relacionadas.");
+			throw new DataIntegrityException("Não é possivel exlcuir porque há pedidos para esse cliente.");
 		}
 
 	}

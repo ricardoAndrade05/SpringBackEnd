1515f62793a8115bf35cecb15f2224a533a34006
Ricardo Andrade <ricardo.andrade05@gmail.com>
2022-07-18 14:20:04 -0300

    Validação customizada: email não repetido na atualização de cliente

--- src/main/java/com/nelioalves/coursomc/dto/ClienteDTO.java
+++ src/main/java/com/nelioalves/coursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.nelioalves.coursomc.domain.Cliente;
+import com.nelioalves.coursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 

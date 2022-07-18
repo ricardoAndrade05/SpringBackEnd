ed08a9a5aab705496fe9ba6a3769a66a3798bd7d
Ricardo Andrade <ricardo.andrade05@gmail.com>
2022-07-18 11:44:26 -0300

    validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/nelioalves/coursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/nelioalves/coursomc/services/validation/ClienteInsertValidator.java
@@ -6,14 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.nelioalves.coursomc.domain.Cliente;
 import com.nelioalves.coursomc.domain.enums.TipoCliente;
 import com.nelioalves.coursomc.dto.ClienteNewDTO;
+import com.nelioalves.coursomc.repositories.ClienteRepository;
 import com.nelioalves.coursomc.resources.exception.FieldMessage;
 import com.nelioalves.coursomc.services.validation.utils.BR;
 
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -23,12 +30,17 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		List<FieldMessage> list = new ArrayList<>();
 		
-		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
-		
-		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "E-mail já existente"));
 		}
 
 		/*

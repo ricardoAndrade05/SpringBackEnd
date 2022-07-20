3810a640206032dc1d9127e04cd94348cf561c80
Ricardo Andrade <ricardo.andrade05@gmail.com>
2022-07-20 14:32:26 -0300

    Criando profile de Desenvolvimento

--- src/main/java/com/nelioalves/coursomc/services/DBService.java
+++ src/main/java/com/nelioalves/coursomc/services/DBService.java
@@ -61,7 +61,7 @@ public class DBService {
 		Categoria cat5 = new Categoria(null, "Jardinagem");
 		Categoria cat6 = new Categoria(null, "Decoração");
 		Categoria cat7 = new Categoria(null, "Perfumaria");
-
+		
 		Produto p01 = new Produto(null, "Computador", 2000.00);
 		Produto p02 = new Produto(null, "Impressora", 800.00);
 		Produto p03 = new Produto(null, "Mouse", 80.00);

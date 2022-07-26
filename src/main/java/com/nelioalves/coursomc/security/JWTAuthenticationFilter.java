6f6b95a7d89f47bc486d637efd999ea8bd8aac57
Ricardo Andrade <ricardo.andrade05@gmail.com>
2022-07-26 19:26:49 -0300

    Implementando Autorização

--- src/main/java/com/nelioalves/coursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/nelioalves/coursomc/security/JWTAuthenticationFilter.java
@@ -25,6 +25,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	private JWTUtil jwtUtil;
 	
 	public JWTAuthenticationFilter(AuthenticationManager authenticationManager,JWTUtil jwtUtil) {
+		setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
 		this.authenticationManager = authenticationManager;
 		this.jwtUtil = jwtUtil;
 	}

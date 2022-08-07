740093d0b462731637f39a8fe42dbee234d7fde7
Ricardo Andrade <ricardo.andrade05@gmail.com>
2022-08-07 17:07:54 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/nelioalves/coursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/coursomc/CursomcApplication.java
@@ -1,12 +1,18 @@
 package com.nelioalves.coursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nelioalves.coursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -14,6 +20,7 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		s3Service.uploadFile("D:\\Udemy - Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB\\07 Armazenamento de imagens usando Amazon S3\\imagens\\Ricardinho.jpg");
 	}
 
 }
